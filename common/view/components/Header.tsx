23d1e785f6728e2c92038d4264c85f8bdc4c11e4
Arnau Gómez Farell <agomez1f@gmail.com>
2021-06-29 08:40:04 +0200

    style(header): buttons layout

--- common/view/components/Header.tsx
+++ common/view/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = (props: HeaderProps): JSX.Element => (
   <header className="sticky w-full px-4 py-2.5 bg-stone text-white">
     <MaxWidth className="flex justify-between items-stretch">
       <h1 className="flex items-center">MiTitulitis.com</h1>
-      <div className="flex justify-between items-stretch space-x-3">
+      <div className="flex justify-between items-stretch space-x-3 py-0.5">
         <Button variant="secondary-white">Log in</Button>
         <Button variant="white" className="text-stone">
           Join
