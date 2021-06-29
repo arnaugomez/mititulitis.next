a4407a77865c81660553f2b09edebe758392e5e1
Arnau Gómez Farell <agomez1f@gmail.com>
2021-06-29 17:05:15 +0200

    fix(banner): problem with layout and banner width

--- news/views/Banner.tsx
+++ news/views/Banner.tsx
@@ -8,25 +8,27 @@ import { useEmblaCarousel } from "embla-carousel/react";
 const Banner = (): JSX.Element => {
   const [emblaRef] = useEmblaCarousel();
   return (
-    <div className="overflow-hidden" ref={emblaRef}>
-      <section className="h-52 flex items-stretch space-x-4">
-        <article className="embla__slide rounded-xl border border-wine-red bg-wine-red-100 py-4 px-5 flex">
-          <div className="flex-1">
-            <h2 className="text-wine-red-800 mt-0">Que nada te pare</h2>
-            <p>
-              Sacarse un título, aprender un idioma, tocar un instrumento...
-              aquí encontrarás los recursos que necesitas para conseguirlo, y la
-              comunidad para seguir motivado cada día.
-            </p>
+    <div className="max-w-full overflow-hidden" ref={emblaRef}>
+      <section className="w-full h-52 flex items-stretch space-x-4">
+        <article className="embla__slide rounded-xl border border-wine-red bg-wine-red-100 relative">
+          <div className="absolute inset-0 py-4 px-5 flex">
+            <div className="flex-1">
+              <h2 className="text-wine-red-800 mt-0">Que nada te pare</h2>
+              <p className="">
+                Sacarse un título, aprender un idioma, tocar un instrumento...
+                aquí encontrarás los recursos que necesitas para conseguirlo, y
+                la comunidad para seguir motivado cada día.
+              </p>
+            </div>
+            <div className="flex-none w-48 relative">
+              <div className="absolute left-0 w-20 animation-float--delay right-0 top-2">
+                <Image src={sphereImg} alt="" />
+              </div>
+              <div className="absolute right-0 w-36 animation-float right-0 top-6">
+                <Image src={helixImg} alt="" />
+              </div>
+            </div>
           </div>
-          <figure className="flex-none w-48 relative">
-            <div className="absolute left-0 w-20 animation-float--delay right-0 top-2">
-              <Image src={sphereImg} alt="" />
-            </div>
-            <div className="absolute right-0 w-36 animation-float right-0 top-6">
-              <Image src={helixImg} alt="" />
-            </div>
-          </figure>
         </article>
         <article className="embla__slide rounded-xl border border-straw-700 bg-straw-100 py-4 px-5 flex">
           <div className="flex-1">
@@ -37,14 +39,14 @@ const Banner = (): JSX.Element => {
               tus amigos!
             </p>
           </div>
-          <figure className="flex-none w-48 relative">
+          <div className="flex-none w-48 relative">
             <div className="absolute right-0 w-36 animation-float right-0 top-6">
               <Image src={thorusImg} alt="" />
             </div>
             <div className="absolute left-0 w-20 animation-float--delay right-2 top-2">
               <Image src={coinImg} alt="" />
             </div>
-          </figure>
+          </div>
         </article>
       </section>
     </div>
